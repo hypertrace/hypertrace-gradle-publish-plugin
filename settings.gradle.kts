82b68f2b48a7c62f6f6d0a477ea91e590bb88b40
Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
2025-06-25 14:24:08 -0400

    chore: update publish plugin to publish to new repo (#28)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,13 +3,13 @@ pluginManagement {
     mavenLocal()
     gradlePluginPortal()
     maven {
-      url = uri("https://hypertrace.jfrog.io/artifactory/maven")
+      url = uri("https://us-maven.pkg.dev/hypertrace-repos/maven")
     }
   }
 }
 
 plugins {
-  id("org.hypertrace.version-settings") version "0.1.1"
+  id("org.hypertrace.version-settings") version "0.2.1"
 }
 
 rootProject.name = "hypertrace-gradle-publish-plugin"
